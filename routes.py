1926a892c2ff03e0f2749e749aa77626695b4da9
smurfik <smurfik@mlpinit.com>
2016-01-21 14:33:21 -0800

    add sending email

--- routes.py
+++ routes.py
@@ -1,7 +1,37 @@
 from flask import Flask, render_template
+from flask_mail import Mail, Message
+import config
+
 app = Flask(__name__)
+
+app.config.update(
+	DEBUG=True,
+	#EMAIL SETTINGS
+	MAIL_SERVER='smtp.mail.yahoo.com',
+	MAIL_PORT=465,
+	MAIL_USE_SSL=True,
+	MAIL_USERNAME = config.MAIL_USERNAME,
+	MAIL_PASSWORD = config.MAIL_PASSWORD
+	)
+mail = Mail(app)
+
 @app.route('/')
 def home():
     return render_template('home.html')
+
+@app.route('/mail')
+def send_mail():
+    try:
+        msg = Message("Send Mail!",
+          sender="tamaramitryakova@yahoo.com",
+          recipients=["tamaramitryakova@yahoo.com"])
+        msg.body = "Yo!\nHave you heard the good word of Python???"
+        mail.send(msg)
+        return 'Mail sent!'
+
+    except Exception, e:
+        return(str(e))
+
+
 if __name__ == '__main__':
     app.run()
